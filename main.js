1b7fd957e49730926de15bb67d25e86d3633f2ed
Stefan Pedratscher <Stefan.Pedratscher@student.uibk.ac.at>
2021-03-15 10:18:00 +0100

    changed to line-by-line reading

--- main.js
+++ main.js
@@ -1,32 +1,52 @@
-
+const fs = require('fs');
+const readline = require('readline');
+const generator = require('./utils/generator')
+const optionsDetector = require('./utils/options')
 
 async function main(project) {
 
-    // Read project file(s)
-    var fs = require('fs');
-    fs.readFile(project, 'utf8', function(err, data) {
-        if (err) throw err;
+    // Creat read interface to read line by line
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream(project),
+    });
 
-        // Parse source file
-        var esprima = require('esprima');
-        var ast = esprima.parseScript(data, { comment: true, loc: true, tolerant: true })
+    // Declare variables
+    var requires = "";
+    var inputs = "";
+    var codeBlock = "";
+    var returnJsonString = "";
+    var inCodeBlock = false;
+    var firstLine = false;
+
+    // Read line by line
+    readInterface.on('line', function(line) {
 
         // Detect code block
-        var detector = require('./utils/detector')
-        var codeBlockStartEnd = detector.detectCodeBlock(ast.comments)
+        if(line.includes('cfunend')){
 
-        // Check if code block is found
-        if(codeBlockStartEnd[0] !== -1 && codeBlockStartEnd[1] !== -1 ){
-            var codeBlock = data.split('\n').splice(codeBlockStartEnd[0], codeBlockStartEnd[1]-codeBlockStartEnd[0]+1)
-
-            var optionsDetector = require('./utils/options')
-            var options = optionsDetector.readCodeBlock(codeBlock[0]);
+            // Create index.js
+            if (!fs.existsSync("out")){
+                fs.mkdirSync("out");
+            }
+            console.log(codeBlock)
+            fs.writeFileSync(
+                "out/aws/index.js",
+                generator.indexAWS(
+                    requires,
+                    inputs,
+                    codeBlock,
+                    returnJsonString
+                )
+            )
+            codeBlock = "";
+            inCodeBlock = false;
+        } else if(line.includes('cfun')){
+            inCodeBlock = true
+            firstLine = true
+            var options = optionsDetector.readCodeBlock(line);
 
             console.log(options)
 
-            var generator = require('./utils/generator')
-
-            var requires = "";
             options.require.forEach(function(value) {
                 var req = value.split(' as ')
                 requires += "const " + req[1] + " = require(\"" + req[0] + "\")\n"
@@ -38,12 +58,11 @@ async function main(project) {
                 }
             });
 
-            var inputs = "";
             options.vars.forEach(function(value) {
                 inputs += "let " + value + " = event." + value + "\n"
             });
 
-            var returnJsonString = "{ ";
+            returnJsonString = "{ ";
             options.assign.forEach(function(value) {
                 returnJsonString += value + ": " + value + ", "
             });
@@ -55,25 +74,16 @@ async function main(project) {
             });
             var pckgGenerator = require('./utils/packageGenerator')
             pckgGenerator.packageGen(options.name, installs)
-
-            // create index.js
-            if (!fs.existsSync("out")){
-                fs.mkdirSync("out");
-            }
-            fs.writeFileSync(
-                "out/aws/index.js",
-                generator.indexAWS(
-                    requires,
-                    inputs,
-                    codeBlock.splice(1, codeBlockStartEnd[1]-codeBlockStartEnd[0]-1).join('\n'),
-                    returnJsonString
-                )
-              )
-
-        } else {
-            console.log('Skipping file ' + project + ". Could not find annotation start or end")
         }
 
+        if(inCodeBlock){
+            if(firstLine){
+                firstLine = false
+            }else{
+                codeBlock += line + "\n";
+            }
+
+        }
     });
 }
 
