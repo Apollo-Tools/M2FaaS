ad55190f23f4019bc52a5dbb27345ed675435bbf
Stefan Pedratscher <Stefan.Pedratscher@student.uibk.ac.at>
2021-03-16 10:54:37 +0100

    read all files in directory

--- main.js
+++ main.js
@@ -20,56 +20,73 @@ const aws = require('./utils/provider/aws');
  */
 async function main(project) {
 
-    // Creat read interface to read line by line
-    const readInterface = readline.createInterface({
-        input: fs.createReadStream(project)
-    });
+    // Read all files in project root
+    fs.readdir('./example', function (err, files) {
 
-    // Initialize variables
-    let requires = '', inputs = '', codeBlock = '', returnJsonString = '', jsonInput = '', funcReturn = '', functionName = '';
-    let inCodeBlock = false, firstLine = false;
-    let startLine = -1, endLine = -1, lines = 0;
+        // Check there was an error
+        if (err) {
+            return console.log('Unable to scan directory: ' + project);
+        }
 
-    // Read line by line
-    readInterface.on('line', function(line) {
+        // Iterate over all files
+        files.forEach(function (file) {
 
-        // Increase total number of lines
-        lines++;
+            file = project + file;
 
-        // Detect code block
-        if(line.includes('cfunend')) {
+            console.log("Checking file \"" + file + "\" ...");
 
-            // End of code block
-            endLine = lines - 1;
+            // Creat read interface to read line by line
+            const readInterface = readline.createInterface({
+                input: fs.createReadStream(file)
+            });
 
-            // Create output directory if it does not exists
-            if (!fs.existsSync("out")){
-                fs.mkdirSync("out");
-            }
+            // Initialize variables
+            let requires = '', inputs = '', codeBlock = '', returnJsonString = '', jsonInput = '', funcReturn = '', functionName = '';
+            let inCodeBlock = false, firstLine = false;
+            let startLine = -1, endLine = -1, lines = 0;
 
-            // Create index.js file for the cloud function
-            fs.writeFileSync(
-                "out/aws/index.js",
-                generator.indexAWS(requires, inputs, codeBlock, returnJsonString)
-            );
+            // Read line by line
+            readInterface.on('line', function(line) {
 
-            // Reset variables
-            codeBlock = '';
-            inCodeBlock = false;
+                // Increase total number of lines
+                lines++;
 
-            // Adapt initial source code and read file
-            fs.readFile(project, 'utf8', function(err, data) {
+                // Detect code block
+                if(line.includes('cfunend')) {
 
-                // Convert file to string[]
-                var fileContent = data.split("\n")
+                    console.log("Porting cloud function " + functionName + " from \"" + file + "\" ...");
 
-                // Comment code block
-                fileContent[startLine] = "/*\n" + fileContent[startLine];
-                fileContent[endLine - 1] = fileContent[endLine - 1] + " */ ";
+                    // End of code block
+                    endLine = lines - 1;
 
-                // Add serverless function call to the monolith
-                var region = 'us-east-1';
-                fileContent[endLine] += "\n" + prettier.format(`
+                    // Create output directory if it does not exists
+                    if (!fs.existsSync("out")){
+                        fs.mkdirSync("out");
+                    }
+
+                    // Create index.js file for the cloud function
+                    fs.writeFileSync(
+                        "out/aws/index.js",
+                        generator.indexAWS(requires, inputs, codeBlock, returnJsonString)
+                    );
+
+                    // Reset variables
+                    codeBlock = '';
+                    inCodeBlock = false;
+
+                    // Adapt initial source code and read file
+                    fs.readFile(file, 'utf8', function(err, data) {
+
+                        // Convert file to string[]
+                        var fileContent = data.split("\n")
+
+                        // Comment code block
+                        fileContent[startLine] = "/*\n" + fileContent[startLine];
+                        fileContent[endLine - 1] = fileContent[endLine - 1] + " */ ";
+
+                        // Add serverless function call to the monolith
+                        var region = 'us-east-1';
+                        fileContent[endLine] += "\n" + prettier.format(`
                     var awsSDK = require('aws-sdk');
                     var credentialsAmazon = new awsSDK.SharedIniFileCredentials({profile: 'default'});
                     let ${functionName}Solution = JSON.parse(await (new (require('aws-sdk'))
@@ -82,80 +99,82 @@ async function main(project) {
                     ${funcReturn}
                     `, { parser: "babel"});
 
-                // Write file back to storage
-                fs.writeFileSync('./out/simpleFunction.js', fileContent.join("\n"));
+                        // Write file back to storage
+                        fs.writeFileSync('./out/simpleFunction.js', fileContent.join("\n"));
 
-                // Generate package.json
-                var pckgGenerator = require('./utils/packageGenerator');
-                pckgGenerator.packageGen(functionName, installs);
+                        // Generate package.json
+                        var pckgGenerator = require('./utils/packageGenerator');
+                        pckgGenerator.packageGen(functionName, installs);
 
-                // Deploy function
-                const AdmZip = require('adm-zip');
-                var zip = new AdmZip();
-                zip.addLocalFile('./out/aws/foo.js');
-                zip.addLocalFile('./out/aws/index.js');
-                zip.addLocalFile('./out/aws/package.json');
-                zip.writeZip('./out/aws/aws.zip');
+                        // Deploy function
+                        const AdmZip = require('adm-zip');
+                        var zip = new AdmZip();
+                        zip.addLocalFile('./out/aws/foo.js');
+                        zip.addLocalFile('./out/aws/index.js');
+                        zip.addLocalFile('./out/aws/package.json');
+                        zip.writeZip('./out/aws/aws.zip');
 
-                aws.deploy(functionName, region);
-            });
+                        aws.deploy(functionName, region);
+                    });
 
-        } else if(line.includes('cfun')){
+                } else if(line.includes('cfun')){
 
-            // Found start of code block
-            startLine = lines;
+                    // Found start of code block
+                    startLine = lines;
 
-            // Detect M2FaaS options
-            var options = optionsDetector.getOptions(line);
+                    // Detect M2FaaS options
+                    var options = optionsDetector.getOptions(line);
 
-            // TODO remove this line
-            console.log(options);
+                    // TODO remove this line
+                    //console.log(options);
 
-            // Set function name
-            functionName = options.name;
+                    // Set function name
+                    functionName = options.name;
 
-            // Handle require option
-            options.require.forEach(function(value) {
-                let requireElement = value.split(' as ')
-                requires += "const " + requireElement[1] + " = require(\"" + requireElement[0] + "\")\n"
+                    // Handle require option
+                    options.require.forEach(function(value) {
+                        let requireElement = value.split(' as ')
+                        requires += "const " + requireElement[1] + " = require(\"" + requireElement[0] + "\")\n"
 
-                // Handle file dependencies for local dependencies
-                if (/^\w+$/.test(requireElement[0]) === false) {
-                    var webpackmanager = require('./utils/webpackmanager')
-                    webpackmanager.bundle("./example/foo.js", requireElement[0].match('[a-zA-Z]*.js')[0])
+                        // Handle file dependencies for local dependencies
+                        if (/^\w+$/.test(requireElement[0]) === false) {
+                            var webpackmanager = require('./utils/webpackmanager')
+                            webpackmanager.bundle("./example/foo.js", requireElement[0].match('[a-zA-Z]*.js')[0])
+                        }
+                    });
+
+                    // Handle "vars" option
+                    jsonInput = "{ ";
+                    options.vars.forEach(function(value) {
+                        inputs += "let " + value + " = event." + value + "\n"
+                        jsonInput += value + ": " + value + ", "
+                    });
+                    jsonInput =  jsonInput.slice(0,-1) + " }";
+
+                    // Handle assign option
+                    returnJsonString = "{ ";
+                    options.assign.forEach(function(value) {
+                        funcReturn += value + " = " + functionName +  "Solution.body." + value + "\n"
+                        returnJsonString += value + ": " + value + ", "
+                    });
+                    returnJsonString =  returnJsonString.slice(0,-1) + " }";
+
+                    // Handle install option
+                    var installs = {};
+                    options.install.forEach(function(value) {
+                        installs[value] = "latest"
+                    });
+                }
+
+                // Save code block
+                inCodeBlock = true;
+                firstLine = true;
+                if(inCodeBlock){
+                    firstLine ? firstLine = false : codeBlock += line + "\n";
                 }
             });
-
-            // Handle "vars" option
-            jsonInput = "{ ";
-            options.vars.forEach(function(value) {
-                inputs += "let " + value + " = event." + value + "\n"
-                jsonInput += value + ": " + value + ", "
-            });
-            jsonInput =  jsonInput.slice(0,-1) + " }";
-
-            // Handle assign option
-            returnJsonString = "{ ";
-            options.assign.forEach(function(value) {
-                funcReturn += value + " = " + functionName +  "Solution.body." + value + "\n"
-                returnJsonString += value + ": " + value + ", "
-            });
-            returnJsonString =  returnJsonString.slice(0,-1) + " }";
-
-            // Handle install option
-            var installs = {};
-            options.install.forEach(function(value) {
-                installs[value] = "latest"
-            });
-        }
-
-        // Save code block
-        inCodeBlock = true;
-        firstLine = true;
-        if(inCodeBlock){
-            firstLine ? firstLine = false : codeBlock += line + "\n";
-        }
+        });
     });
 }
 
-main("example/simpleFunction.js")
+main("example/")
