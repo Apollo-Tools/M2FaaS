7c228f8561ad9b00fc12c8e6e1cbb9822d9d4f43
Stefan Pedratscher <Stefan.Pedratscher@student.uibk.ac.at>
2021-04-06 15:08:53 +0200

    adapted example

--- example/example.js
+++ example/example.js
@@ -4,9 +4,10 @@ const _ = require('lodash')
 async function main(args) {
     let a = 2;
 
-    // cfun name(m2faasExample) require(./foo.js as foo,lodash as _) assign(value,a,fooBefore) vars(a) install(lodash) deploy([{"name": "m2FaaSExampleAWS", "provider": "aws", "region": "us-east-1", "memorySize": 128, "runtime": "nodejs14.x", "timeout": 30, "role": "arn:aws:iam::170392512081:role/service-role/getFlight-role-n1g2o34s"},{"name": "m2FaaSExampleIBM", "provider": "ibm", "region": "eu-gb", "memorySize": 128, "runtime": "nodejs:12", "timeout": 60 }])
-    var fooBefore = foo.fun(a);
-    a = 122;
+    // cfun name(m2faasExample) require(./foo.js as foo,lodash as _) assign(value,a,fooBefore) vars(a) install(lodash) deploy([{"name": "m2FaaSExampleAWS", "provider": "aws", "region": "us-east-1", "memorySize": 128, "runtime": "nodejs14.x", "timeout": 3, "role": "arn:aws:iam::170392512081:role/service-role/getFlight-role-n1g2o34s"},{"name": "m2FaaSExampleIBM", "provider": "ibm", "region": "eu-gb", "memorySize": 128, "runtime": "nodejs:12", "timeout": 60 }])
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    const fooBefore = foo.fun(a);
+    a = 43;
     const value = _.chunk(['a', 'b', 'c', 'd'], 2);
     // cfunend
 
