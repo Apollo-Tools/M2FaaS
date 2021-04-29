39411a96e47e696c1e7ae44d4fbb5190cbfc424d
Stefan Pedratscher <Stefan.Pedratscher@student.uibk.ac.at>
2021-04-29 11:05:03 +0200

    added gca

--- example_gca/gca.js
+++ example_gca/gca.js
@@ -1,108 +1,70 @@
-const utils = require("./utils")
 const AWS = require('aws-sdk');
+const Cloudant = require('@cloudant/cloudant');
+const Utils = require("./utils");
+const Config = require("./config");
 
-async function getPassengers(params){
-    const flightId = params.flightID;
+async function getPassengers(flightID){
 
     // Connect to Cloudant and select database
-    const Cloudant = require('@cloudant/cloudant');
-    const cloudant = new Cloudant({
-        url: 'https://d74fd924-41ec-4c4f-bd37-41d352264457-bluemix.cloudantnosqldb.appdomain.cloud/',
-        plugins: {iamauth: {iamApiKey: '9u_dAgheD93UZZK6FUF_o3Jr1Dzy_ogHZa7QBYPvUA2u'}}
-    });
+    const cloudant = new Cloudant(Config.ibm_config);
     const db = cloudant.db.use('passenger');
 
     // Get all documents from the DB
     let allDocs = null;
-    await db.list({include_docs:true}).then(function(data) {
-        allDocs = data;
-    }).catch(function(err) {
-        console.log('something went wrong', err);
-    });
+    await db.list({ include_docs: true }).then(function(data) { allDocs = data; });
 
-    // Define variables for output
-    let passengerIDs = []
-
-    // Iterate over the data
+    let passengerIDs = [];
     for (let i = 0; i < allDocs.rows.length; i++) {
-        if(allDocs.rows[i].doc.atAirport && allDocs.rows[i].doc.flightId === flightId){
+        if(allDocs.rows[i].doc.atAirport && allDocs.rows[i].doc.flightId === flightID){
             passengerIDs.push(allDocs.rows[i].id);
         }
     }
-    return {
-        passengerIDs: passengerIDs,
-        passengersAtAirport: passengerIDs.length
-    };
+    return { passengerIDs: passengerIDs, passengersAtAirport: passengerIDs.length };
 }
 
-async function secCheckTime(args){
-    /*const bucket = 'gcaimages' // the bucketname without s3://
-    const securityGPS = args.security;
+async function secCheckTime(securityGPS){
+    const start = Date.now()
+    AWS.config = new AWS.Config(Config.aws_config);
     const client = new AWS.Rekognition();
-    const params = {
-        Image: {
-            S3Object: {
-                Bucket: bucket,
-                Name: "security_"+securityGPS+".jpg"
-            },
-        },
-        MaxLabels: 10
-    }
-    var finished= false;
-    var array = [];
-    var result = await client.detectLabels(params, function(err, response) {
-        if (err) {
-            console.log(err, err.stack); // an error occurred
-        } else {
-            response.Labels.forEach(label => {
-            }) // for response.labels
-        } // if
+    const params = { Image: { S3Object: { Bucket: 'gcacameraimages', Name: "security_"+securityGPS+".jpg" }, }, MaxLabels: 100 }
+    const array = [];
+    await client.detectLabels(params, function (err, response) {
+        if (err) { console.log(err, err.stack);
+        } else { response.Labels.forEach(label => {}) }
     }).promise().then(data => {
         array.push(data.Labels);
     }).catch(error => {
-        console.log(error);
         return error;
     });
-    var persons = 0;
-    var delay=0;
+    let persons = 0, delay = 0;
     try {
         array[0].forEach(function (el){
             if (el.Name === "Person"){
                 persons = el.Instances.length;
             }
         });
-        //each person takes 2 mins
-        delay = persons * 2;
-    }catch (e) {
-        //ignored
-    }
-    //the delay in minutes
-    return {securityDelay: delay};*/
-    return {securityDelay: 12};
+
+        // Three minutes per person
+        delay = persons * 3;
+    }catch (e) {}
+
+    console.log(Date.now() - start)
+
+    return delay;
 }
 
-async function getGateGPS(params){
-    const gate = params.newGate;
+async function getGateGPS(gate){
 
     // Connect to Cloudant and select database
-    const Cloudant = require('@cloudant/cloudant');
-    const cloudant = new Cloudant({
-        url: 'https://d74fd924-41ec-4c4f-bd37-41d352264457-bluemix.cloudantnosqldb.appdomain.cloud/',
-        plugins: {iamauth: {iamApiKey: '9u_dAgheD93UZZK6FUF_o3Jr1Dzy_ogHZa7QBYPvUA2u'}}
-    });
+    const cloudant = new Cloudant(Config.ibm_config);
     const db = cloudant.db.use('gate');
 
     // Get all documents from the DB
     let allDocs = null;
-    await db.list({include_docs:true}).then(function(data) {
-        allDocs = data;
-    }).catch(function(err) {
-        console.log('something went wrong', err);
-    });
+    await db.list({include_docs:true}).then(function(data) { allDocs = data; });
 
     // Prepare inputs
-    let gateGPS = "";
-    let secGPS = "";
+    let gateGPS = "", secGPS = "";
 
     // Iterate over the data
     for (let i = 0; i < allDocs.rows.length; i++) {
@@ -113,7 +75,7 @@ async function getGateGPS(params){
     }
 
     return {
-        newGateGPS: gateGPS,
+        gateGPS: gateGPS,
         securityGPS: secGPS
     };
 }
@@ -121,10 +83,8 @@ async function getGateGPS(params){
 async function distanceGPS(params){
     const gps1 = params.gps1;
     const gps2 = params.gps2;
-    const newGateGPS = params.newGateGPS;
-    const securityGPS = params.securityGPS;
 
-    const distanceInM = utils.distanceInMBetweenEarthCoordinates(
+    const distanceInM = Utils.distanceInMBetweenEarthCoordinates(
         Number(gps1.split(", ")[0]),
         Number(gps1.split(", ")[1]),
         Number(gps2.split(", ")[0]),
@@ -134,22 +94,15 @@ async function distanceGPS(params){
     const delayInMin = delayInSec / 60;
 
     return {
-        newGateGPSOut: newGateGPS,
-        securityGPSOut: securityGPS,
-        delay: delayInMin,
-        delayArea: "{\n\"area\":" + params.area + ",\n\"delay\":" + delayInMin + ",\n}"
+        "area": params.area,
+        "delay": delayInMin
     };
 }
 
-async function readGPS(params){
-    const passengerId = params.passengerID;
+async function readGPS(passengerId){
 
     // Connect to Cloudant and select database
-    const Cloudant = require('@cloudant/cloudant');
-    const cloudant = new Cloudant({
-        url: 'https://d74fd924-41ec-4c4f-bd37-41d352264457-bluemix.cloudantnosqldb.appdomain.cloud/',
-        plugins: {iamauth: {iamApiKey: '9u_dAgheD93UZZK6FUF_o3Jr1Dzy_ogHZa7QBYPvUA2u'}}
-    });
+    const cloudant = new Cloudant(Config.ibm_config);
     const db = cloudant.db.use('passenger');
 
     // Get doc by id
@@ -159,48 +112,30 @@ async function readGPS(params){
     passengerGPS.push(Number(passenger.gpsLocation.split(", ")[1]));
 
     // Check if passenger inside security check
-    const afterSecCheck = utils.inside(
+    const afterSecCheck = Utils.inside(
         passengerGPS,
         [
-            [47.25756215011163, 11.350859214052116],
-            [47.25783976426809, 11.350793499933715],
-            [47.25793624622426, 11.351679969979884],
-            [47.25766682447269, 11.351745684098283]
-        ]
+                [47.25756215011163, 11.350859214052116],
+                [47.25783976426809, 11.350793499933715],
+                [47.25793624622426, 11.351679969979884],
+                [47.25766682447269, 11.351745684098283]
+            ]
     );
 
     let area = 1;
-    if(afterSecCheck){
-        area = 0;
-    }
+    if(afterSecCheck){ area = 0; }
 
     return {
-        passengerIDsOut: params.passengerID,
-        /*gpsGateOut: params.gpsGate,
-        secGateDelayOut: params.secGateDelay,
-        securityDelayOut: params.securityDelay,
-        securityGPSOut: params.securityGPS,*/
         passengerGPS: passenger.gpsLocation,
         area: area
     };
 }
 
-async function sumUp(params){
-    const delay = Number(params.passSecDelay) + Number(params.securityDelay) + Number(params.securityGateDelay);
-    return {
-        delayArea: "{\n\"area\":" + params.area + ",\n\"delay\":"+ delay + ",\n}"
-    };
-}
-
-async function averageTime(params){
-    const jsonStringArray = params.delay;
-    let avgTimePublic = 0;
-    let avgTimeRestricted = 0;
-    let totalPublic = 0;
-    let totalRestricted = 0;
-    for (let i = 0; i < jsonStringArray.length; i++) {
-        const area = Number(jsonStringArray[i].split("area\":")[1].split(",")[0]);
-        const delay = Number(jsonStringArray[i].split("delay\":")[1].split(",")[0]);
+async function averageTime(results){
+    let avgTimePublic = 0, avgTimeRestricted = 0, totalPublic = 0, totalRestricted = 0;
+    for (let i = 0; i < results.length; i++) {
+        const area = Number(results[i].area);
+        const delay = Number(results[i].delay);
         if(area === 0){
             avgTimeRestricted += delay;
             totalRestricted++;
@@ -209,7 +144,7 @@ async function averageTime(params){
             totalPublic++;
         }
     }
-    console.log("in")
+
     return {
         avgTimePublic: avgTimePublic/totalPublic,
         avgTimeRestricted: avgTimeRestricted/totalRestricted
@@ -219,35 +154,37 @@ async function averageTime(params){
 async function main(input){
 
     /* Parallel */
-    const promGetPassengers = getPassengers(input);
-    const promSecCheckTime = secCheckTime(); // TODO
-    const promGetGateGPS = getGateGPS(input);
+    const promGetPassengers = getPassengers(input.flightID);
+    const promSecCheckTime = secCheckTime(input.securityGPS); // TODO
+    const promGetGateGPS = getGateGPS(input.gateID);
     const resGetGateGPS = await promGetGateGPS;
     const promDistanceGPS = distanceGPS({
-        "gps1": resGetGateGPS.newGateGPS,
+        "gps1": resGetGateGPS.gateGPS,
         "gps2": resGetGateGPS.securityGPS,
-        "newGateGPS": resGetGateGPS.newGateGPS,
-        "securityGPS": resGetGateGPS.securityGPS
     });
     const resGetPassengers = await promGetPassengers;
     const resSecCheckTime = await promSecCheckTime;
     const resDistanceGPS = await promDistanceGPS;
 
+    console.log("resGetGate: " + JSON.stringify(resGetGateGPS))
+    console.log("resGetPassengers: " + JSON.stringify(resGetPassengers))
+    console.log("resSecCheck: " + JSON.stringify(resSecCheckTime))
+    console.log("resDisGPS: " + JSON.stringify(resDistanceGPS))
+
     /* ParallelFor */
     let avg = await Promise.all(resGetPassengers.passengerIDs.map(function (node) {
         return new Promise(async function (resolve, reject) {
-            const resReadGPS = await readGPS({"passengerID": node});
+            const resReadGPS = await readGPS(node);
+            console.log("resreadGPS: " + JSON.stringify(resReadGPS))
             if(resReadGPS.area === 1){
-                const resDistanceGPS = await distanceGPS({
+                const resPassSec = await distanceGPS({
                         "gps1": resGetGateGPS.securityGPS,
                         "gps2": resReadGPS.passengerGPS,
                         "area": resReadGPS.area
                     });
-                const resSumUp = await sumUp({
-                    "passSecDelay": 12,
-                    "securityGateDelay": 122
-                });
-                resolve(resSumUp)
+                resolve({ "area": resReadGPS.area, "delay": (Number(resPassSec.delay) + Number(resSecCheckTime) + Number(resDistanceGPS.delay)) })
+                console.log("resPassSec: " + JSON.stringify(resPassSec))
+                console.log("resSumUp: " + JSON.stringify(resSumUp))
             }else{
                 const resDistanceGPS = await  distanceGPS({
                     "gps1": resGetGateGPS.securityGPS,
@@ -255,20 +192,21 @@ async function main(input){
                     "area": resReadGPS.area
                 })
                 resolve(resDistanceGPS);
+                console.log("resDistanceGPS: " + JSON.stringify(resDistanceGPS))
             }
         })
     })).then(function (results) {
-        console.log("res"  + JSON.stringify(results))
+        console.log("result: "  + JSON.stringify(results))
         return averageTime(results);
     }).catch(function (error) {
         // handle error
     })
 
-    console.log(avg + "-----------------------------------")
+    return avg;
 }
 
 main({
     "flightID": "F121",
-    "newGate": "G1",
-    "security": "47.25762807043091, 11.351422424940244"
-})
+    "gateID": "G1",
+    "securityGPS": "47.25762807043091, 11.351422424940244"
+}).then(r => console.log("Result: " + JSON.stringify(r)))
